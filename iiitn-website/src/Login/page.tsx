56e11caf7decc46fcaa3f69c29d079d740c58ea9
Dhruv Anand <ananddhruv29@gmail.com>
2025-02-21 23:27:00 +0530

    Add LoginForm component and update Login page layout; include placeholder image

--- iiitn-website/src/Login/page.tsx
+++ iiitn-website/src/Login/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { LoginForm } from "@/components/form/login-form";
 
 function Login() {
-    return (
-        <div>Login</div>
-    )
+	return (
+		<div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+			<div className="w-full max-w-sm md:max-w-3xl">
+				<LoginForm />
+			</div>
+		</div>
+	);
 }
 
-export default Login
+export default Login;
