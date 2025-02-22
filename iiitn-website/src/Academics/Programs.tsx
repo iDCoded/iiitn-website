15a5f0fd97cafde5edc65e43659b34a19afcdb6d
praneeth-2602 <praneeth.palugula@gmail.com>
2025-02-23 00:10:02 +0530

    Refactor Courses and Programs components for consistent formatting; adjust HeroSection background positioning

--- iiitn-website/src/Academics/Programs.tsx
+++ iiitn-website/src/Academics/Programs.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 function Programs() {
-  return (
-    <div className="bg-gray-100 min-h-screen flex flex-col">
+	return (
+		<div className="bg-gray-100 min-h-screen flex flex-col">
 			{/* Header Section */}
 			<div className="bg-[#002147] text-white py-16 text-center">
 				<h1 className="text-4xl font-bold">Programs</h1>
@@ -10,8 +9,8 @@ function Programs() {
 					Stay updated with the latest consultancy services from IIIT Nagpur.
 				</p>
 			</div>
-        </div>
-  )
+		</div>
+	)
 }
 
 export default Programs
