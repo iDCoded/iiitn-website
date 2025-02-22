73b889c7700e5781a3e7221cbd4f26670448ed02
Dhruv Anand <ananddhruv29@gmail.com>
2025-02-23 01:35:17 +0530

    Implement Signup page and refactor Login component to use SignupForm

--- iiitn-website/src/Login/page.tsx
+++ iiitn-website/src/Login/page.tsx
@@ -1,13 +1,11 @@
-import { LoginForm } from "@/components/form/login-form";
+import Signup from "@/components/auth/signup";
 
-function Login() {
+const LoginPage = () => {
 	return (
-		<div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-			<div className="w-full max-w-sm md:max-w-3xl">
-				<LoginForm />
-			</div>
+		<div>
+			<Signup />
 		</div>
 	);
-}
+};
 
-export default Login;
+export default LoginPage;
