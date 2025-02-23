1af51b69724cb923ecf3348b987415bc6b77532a
Dhruv Anand <ananddhruv29@gmail.com>
2025-02-23 12:10:55 +0530

    DO NOT IGNORE EXCLUDED ROUTES

--- iiitn-website/src/main.tsx
+++ iiitn-website/src/main.tsx
@@ -19,7 +19,12 @@ export function App() {
 				{!isExcludedRoute && <Navbar />}
 
 				{/* Main Content */}
-				<div className="flex-grow pt-20 md:pt-24 lg:pt-32"> {/* Adjust padding for different screen sizes */}
+				<div
+					className={`${
+						isExcludedRoute ? "" : "flex-grow pt-20 md:pt-24 lg:pt-32"
+					}`}>
+					{" "}
+					{/* Adjust padding for different screen sizes */}
 					<AppRoutes />
 				</div>
 
