9b4288bb8230dc592c537231a9a3e05993a4dc0c
Dhruv Anand <ananddhruv29@gmail.com>
2025-02-23 21:41:50 +0530

    Add TypeScript interfaces for Person and Faculty entities

---
+++ iiitn-website/src/interfaces/types.ts
@@ -0,0 +1,16 @@
+export interface Person {
+	name: string;
+	email: string;
+	phone?: string;
+	imageUrl?: string;
+	department: string;
+}
+
+export interface Faculty {
+	f_id: number;
+	p_id: number;
+	join_year: number;
+	positions: string;
+	f_or_s: string;
+	person: Person;
+}
