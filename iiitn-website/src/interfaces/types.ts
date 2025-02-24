c00600784c1e8a40719bcb8e8ef9b8afdbee7e37
Dhruv Anand <ananddhruv29@gmail.com>
2025-02-24 15:08:30 +0530

    Add Card type definition for enhanced card management functionality

--- iiitn-website/src/interfaces/types.ts
+++ iiitn-website/src/interfaces/types.ts
@@ -15,3 +15,20 @@ export interface Faculty {
 	f_or_s: "Faculty" | "Staff";
 	person: Person;
 }
+
+export type Card = {
+	c_id: string;
+	title: string;
+	caption: string;
+	content: string;
+	location: string;
+	c_category: string;
+	c_sub_category: string;
+	date: Date;
+	media_img_id: number;
+	media_vid_id: number;
+	media_doc_id: number;
+	updated_by: number;
+	added_by: number;
+	media?: FileList;
+};
