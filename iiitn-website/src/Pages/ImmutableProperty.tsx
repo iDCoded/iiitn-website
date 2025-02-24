555768d409c335f5fab8297e83ca9772a7ce3f66
lokesh-22 <lokeshboddupally13@gmail.com>
2025-02-25 01:05:43 +0530

    pages

---
+++ iiitn-website/src/Pages/ImmutableProperty.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ImmutableProperty() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex flex-col">
+      {/* Header */}
+      <header className="bg-[#002147] text-white py-16 text-center">
+        <h1 className="text-5xl font-bold">Immutable Properties Details</h1>
+
+        <p className="text-lg mt-2 italic">"Ensuring a safe and inclusive environment for all."</p>
+      </header>
+
+    </div>
+  )
+}
+
+export default ImmutableProperty
