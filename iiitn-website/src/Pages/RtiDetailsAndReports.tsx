602fd5750b1eced8c476834fb9ce918be78d8f30
lokesh-22 <lokeshboddupally13@gmail.com>
2025-02-25 01:51:15 +0530

    pages

--- iiitn-website/src/Pages/RtiDetailsAndReports.tsx
+++ iiitn-website/src/Pages/RtiDetailsAndReports.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function RtiDetailsAndReports() {
   return (
-    <div>RtiDetailsAndReports</div>
+    <div className="bg-gray-100 min-h-screen flex flex-col">
+      {/* Header Section */}
+      <header className="bg-[#002147] text-white py-16 text-center">
+        <h1 className="text-5xl font-bold">RTI Details & Reports</h1>
+        <p className="text-lg mt-2 italic">
+          "Navigate your way to IIIT Nagpur with ease."
+        </p>
+      </header>
+    
+    </div>
   )
 }
 
