9a002d7a2b8bbce6f7f946c85116a891bc1db6c7
praneeth-2602 <praneeth.palugula@gmail.com>
2025-02-25 09:55:05 +0530

    Refactor DetailedNews component and enhance AlumniCarousel with event images

--- iiitn-website/src/components/AlumniCarousel.tsx
+++ iiitn-website/src/components/AlumniCarousel.tsx
@@ -8,17 +8,45 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "../components/ui/carousel"
+import alum1 from "../assets/alum1.jpeg"
+import alum2 from "../assets/alum2.jpeg"
+
+
+const events = [
+  {
+    title: "Alumni Meet 2022",
+    date: "August 15, 2022",
+    description: "Reconnect with old friends and network with fellow alumni.",
+    image: alum1,
+  },
+  {
+    title: "Alumni Meet 2023",
+    date: "July 22, 2023",
+    description: "Experts from various industries will guide the students.",
+    image: alum2,
+  },
+  {
+    title: "Alumni Meet 2022",
+    date: "August 15, 2022",
+    description: "Reconnect with old friends and network with fellow alumni.",
+    image: alum1,
+  },
+]
 
 export function CarouselDemo() {
   return (
-    <Carousel className="w-full max-w-xs">
+    <Carousel className="w-full max-w-screen mx-auto mt-10">
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem key={index}>
             <div className="p-1">
               <Card>
                 <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
+                  <img
+                    src={events[index % events.length].image}
+                    alt={events[index % events.length].title}
+                    className="w-full h-full object-cover rounded-lg"
+                  />
                   
                 </CardContent>
               </Card>
