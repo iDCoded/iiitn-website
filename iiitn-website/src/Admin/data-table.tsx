c331b536ad9d2c8f730ced58f2cd948472b29762
Dhruv Anand <ananddhruv29@gmail.com>
2025-02-26 12:37:49 +0530

    Add sorting state to data table

--- iiitn-website/src/Admin/data-table.tsx
+++ iiitn-website/src/Admin/data-table.tsx
@@ -3,6 +3,7 @@ import {
 	flexRender,
 	getCoreRowModel,
 	getSortedRowModel,
+	SortingState,
 	useReactTable,
 } from "@tanstack/react-table";
 
@@ -14,6 +15,7 @@ import {
 	TableHeader,
 	TableRow,
 } from "@/components/ui/table";
+import { useState } from "react";
 
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[];
@@ -24,11 +26,17 @@ export function DataTable<TData, TValue>({
 	columns,
 	data,
 }: DataTableProps<TData, TValue>) {
+	const [sorting, setSorting] = useState<SortingState>([]);
+
 	const table = useReactTable({
 		data,
 		columns,
 		getCoreRowModel: getCoreRowModel(),
 		getSortedRowModel: getSortedRowModel(),
+		onSortingChange: setSorting,
+		state: {
+			sorting,
+		},
 	});
 
 	return (
