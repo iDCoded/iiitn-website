d8148a5bff30adc0a5622493b512f394bae1c25c
praneeth-2602 <praneeth.palugula@gmail.com>
2025-03-03 16:02:12 +0530

    Implement AlumniSection component with animated testimonials and integrate into TestimonialsSection

--- iiitn-website/src/components/Testimonials.tsx
+++ iiitn-website/src/components/Testimonials.tsx
@@ -2,7 +2,7 @@ import AlumniSection from "./AlumniSection";
 
 const TestimonialsSection = () => {
 	return (
-		<section className="relative flex flex-col items-center space-y-10 h-screen px-4 md:px-10 justify-center bg-background">
+		<section className="relative flex flex-col items-center space-y-10 h-screen justify-center bg-background">
 			<AlumniSection/>
 		</section>
 	);
