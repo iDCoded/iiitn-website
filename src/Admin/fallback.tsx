87358a60f6ac464ee69f0e2d1d5a829c3cc0c56d
Dhruv Anand <ananddhruv29@gmail.com>
2025-03-05 13:46:54 +0530

    Enhance Fallback component with Accordion for detailed error display

--- src/Admin/fallback.tsx
+++ src/Admin/fallback.tsx
@@ -1,3 +1,9 @@
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import { Frown } from "lucide-react";
 
@@ -9,8 +15,23 @@ function Fallback({ error }: { error?: Error }) {
 				<h1 className="text-4xl font-bold leading-tight">An Error Occured</h1>
 				<p className="text-center text-muted-foreground">
 					Oops! Something went wrong. <br />
-					{error && <span className="text-red-500">{error.message}</span>}
 				</p>
+				{/* Accordion for showing error details */}
+				{error && (
+					<Accordion
+						type="single"
+						collapsible
+						className="w-[90%] max-w-md text-red-600">
+						<AccordionItem value="error-details">
+							<AccordionTrigger>Error Details</AccordionTrigger>
+							<AccordionContent>
+								<div className="overflow-auto max-h-60 bg-red-100 p-3 rounded-md text-sm text-red-600">
+									{error.message}
+								</div>
+							</AccordionContent>
+						</AccordionItem>
+					</Accordion>
+				)}
 				<Button>Go Back</Button>
 			</div>
 		</div>
