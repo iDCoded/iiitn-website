b2a7e5124899452f880c9dbbfd48d90afc2730bf
Dhruv Anand <ananddhruv29@gmail.com>
2025-03-08 20:56:34 +0530

    Add dialog for creating new publications in faculty publications table

--- src/Faculty/publications/page.tsx
+++ src/Faculty/publications/page.tsx
@@ -2,7 +2,16 @@ import { useEffect, useState } from "react";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 import { Publication } from "@/interfaces/types";
-import { MediaUpload } from "@/Admin/media/media-upload-dialog";
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import PublicationForm from "../publication-form";
 
 function FacultyPublicationsTablePage() {
 	const [pubData, setPubData] = useState<Publication[]>([]);
@@ -38,7 +47,20 @@ function FacultyPublicationsTablePage() {
 	return (
 		<div className="container mx-auto py-10 h-screen">
 			<div className="flex flex-col items-end gap-8">
-				<MediaUpload />
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button variant={"outline"} size={"lg"}>
+							<Plus strokeWidth={4} />
+							<p>Add Publication</p>
+						</Button>
+					</DialogTrigger>
+					<DialogContent className="max-h-[80vh] overflow-y-auto">
+						<DialogHeader>
+							<DialogTitle>Create a Publication</DialogTitle>
+						</DialogHeader>
+						<PublicationForm />
+					</DialogContent>
+				</Dialog>
 				<DataTable columns={columns} data={pubData} />
 			</div>
 		</div>
