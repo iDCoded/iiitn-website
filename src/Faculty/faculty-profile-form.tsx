3354a08a1723581672e2b44cac3c7ab251f34ea0
Dhruv Anand <ananddhruv29@gmail.com>
2025-03-10 00:19:52 +0530

    Add uploading profile picture for faculty

--- src/Faculty/faculty-profile-form.tsx
+++ src/Faculty/faculty-profile-form.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { User } from "lucide-react";
+import { ImageIcon, User } from "lucide-react";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -24,9 +24,12 @@ import MDEditor from "@uiw/react-md-editor";
 const formSchema = z.object({
 	f_id: z.string(),
 	p_id: z.string(),
-	d_id: z.string(),
+	b_id: z.string(),
 	pub_id: z.string(),
 	media_img_id: z.string(),
+	media: z
+		.instanceof(FileList)
+		.refine((fileList) => fileList.length > 0, { message: "No file selected" }),
 	join_year: z.coerce.number().int().gte(1900).lte(new Date().getFullYear()),
 	content: z.string().min(1, "Content is required"),
 	positions: z.string().min(1, { message: "Please enter the positions" }),
@@ -45,9 +48,10 @@ export default function FacultyForm() {
 		defaultValues: {
 			f_id: "",
 			p_id: "",
-			d_id: (Math.floor(Math.random() * 2) + 1).toString(), // Random number b/w 1 - 2.
+			b_id: "1",
 			pub_id: "",
 			media_img_id: "",
+			media: undefined,
 			content: "",
 			join_year: new Date().getFullYear(),
 			positions: "",
@@ -59,34 +63,59 @@ export default function FacultyForm() {
 		},
 	});
 
-	const handleSubmit = async (data: z.infer<typeof formSchema>) => {
-		const facultyData = {
-			p_id: 1, // ! Remove in production -> Get user ID from logged in user
-			d_id: (Math.floor(Math.random() * 2) + 1).toString(), // ! Remove in production -> Random number b/w 1 - 2
-			pub_id: "1",
-			media_img_id: 1,
-			join_year: data.join_year,
-			positions: data.positions,
-			f_or_s: data.f_or_s,
-			education: data.education,
-			experience: data.experience,
-			teaching: data.teaching,
-			research: data.research,
-			content: data.content,
-		};
+	const fileRef = form.register("media");
 
-		const res = await fetch(
-			`${import.meta.env.VITE_API_BASE_URL}/faculty/faculty_staff`,
-			{
-				method: "PUT",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(facultyData),
+	const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+		// Upload the profile picture and get it's media ID
+		const file = data.media[0];
+
+		const formData = new FormData();
+		formData.append("file", file);
+		formData.append("media_type", file.name);
+
+		try {
+			const media_request = await fetch(
+				`${import.meta.env.VITE_API_BASE_URL}/media/upload`,
+				{
+					method: "POST",
+					body: formData,
+				}
+			);
+			const media_res = await media_request.json();
+
+			if (media_request.ok) {
+				const facultyData = {
+					p_id: 1, // ! Remove in production -> Get user ID from logged in user
+					b_id: 1, // ! Remove in production
+					pub_id: "1",
+					media_img_id: media_res.media_img_id,
+					join_year: data.join_year,
+					positions: data.positions,
+					f_or_s: data.f_or_s,
+					education: data.education,
+					experience: data.experience,
+					teaching: data.teaching,
+					research: data.research,
+					content: data.content,
+					media: data.media,
+				};
+
+				const res = await fetch(
+					`${import.meta.env.VITE_API_BASE_URL}/faculty/faculty_staff/${1}`, // ! Faculty ID is hardcoded
+					{
+						method: "PUT",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify(facultyData),
+					}
+				);
+				const res_json = await res.json();
+				console.log("res_json", res_json);
 			}
-		);
-		const res_json = await res.json();
-		console.log("res_json", res_json);
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
 	return (
@@ -96,13 +125,15 @@ export default function FacultyForm() {
 					<User className="h-6 w-6 text-primary" />
 				</div>
 				<div>
-					<h2 className="text-2xl font-semibold">Create faculty</h2>
-					<p className="text-sm text-gray-500">Add a new faculty member</p>
+					<h2 className="text-2xl font-semibold">Enter your details</h2>
+					<p className="text-sm text-gray-500">Specify your details</p>
 				</div>
 			</div>
 
 			<Form {...form}>
-				<form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
+				<form
+					onSubmit={form.handleSubmit(handleSubmit)}
+					className="space-y-6 pb-6">
 					<FormField
 						control={form.control}
 						name="join_year"
@@ -113,6 +144,54 @@ export default function FacultyForm() {
 									<Input id="join_year" placeholder="Enter year" {...field} />
 								</FormControl>
 								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="media"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Profile Picture</FormLabel>
+								<FormControl>
+									<div className="flex flex-col gap-4">
+										<div className="flex items-center gap-3">
+											<Input
+												type="file"
+												id={field.name}
+												accept="image/*"
+												{...fileRef}
+												placeholder="Upload Media"
+											/>
+											<Button
+												type="button"
+												variant="outline"
+												onClick={() =>
+													document.getElementById(field.name)?.click()
+												}>
+												<ImageIcon className="mr-2 h-4 w-4" />
+												Browse
+											</Button>
+										</div>
+										{field.value && field.value[0].type.includes("image") && (
+											<div className="rounded-lg border bg-card">
+												<img
+													src={
+														URL.createObjectURL(field.value[0]) ||
+														"/placeholder.svg"
+													}
+													alt="Preview"
+													className="w-full h-[200px] object-cover rounded-t-lg"
+												/>
+												<div className="p-3">
+													<p className="text-sm text-gray-500">
+														Selected file: {field.value[0].name}
+													</p>
+												</div>
+											</div>
+										)}{" "}
+									</div>
+								</FormControl>
 							</FormItem>
 						)}
 					/>
